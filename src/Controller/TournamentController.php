c392b9f4d9796f4eed84257f2472f7ae8222ca7d
SoczuKS <soczynskik@gmail.com>
2021-05-07 11:20:05 +0200

    Add empty TournamentController

---
+++ src/Controller/TournamentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TournamentController extends AbstractController
+{
+    /**
+     * @Route("/tournaments", name="tournaments")
+     */
+    public function index(): Response
+    {
+        return $this->render('tournament/index.html.twig', [
+            'controller_name' => 'TournamentController',
+        ]);
+    }
+}
