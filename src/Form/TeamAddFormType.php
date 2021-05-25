16912cc71b833fd5884ff619889a8aff1b590fe8
SoczuKS <soczynskik@gmail.com>
2021-05-26 01:00:16 +0200

    Team add form css

--- src/Form/TeamAddFormType.php
+++ src/Form/TeamAddFormType.php
@@ -4,7 +4,7 @@ namespace App\Form;
 
 use App\Entity\Team;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,6 +15,7 @@ class TeamAddFormType extends AbstractType
         $builder
             ->add('name')
             ->add('address')
+            ->add('add', SubmitType::class);
         ;
     }
 
