7d4194b8b93cab3588feb18143f2e0731e7b6beb
SoczuKS <soczynskik@gmail.com>
2021-06-16 15:36:36 +0200

    Make team row clickable (go to selected team page)

--- assets/teams.js
+++ assets/teams.js
@@ -8,4 +8,15 @@ $(document).ready(() => {
     $("#team_add_new_address_new_city_button").click(() => {
         $("#team_add_new_address_new_city_form_div").css("visibility", "visible");
     });
+
+    $(".clickable_row").click(function () {
+        let url = $(this).data("href");
+
+        if (url && url.length > 0) {
+            console.log('P1');
+            document.location.href = url;
+
+            return false;
+        }
+    });
 });
