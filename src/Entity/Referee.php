bba349a3847fc2ccc9d1a4db4486b62e21e5e923
SoczuKS <soczynskik@gmail.com>
2022-04-19 22:28:14 +0200

    Add User entity

--- src/Entity/Referee.php
+++ src/Entity/Referee.php
@@ -19,6 +19,9 @@ class Referee
     #[ORM\Column(type: 'string', length: 255)]
     private string $surname;
 
+    #[ORM\OneToOne(mappedBy: 'referee', targetEntity: User::class, cascade: ['persist', 'remove'])]
+    private $user;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -44,6 +47,28 @@ class Referee
     public function setSurname(string $surname): self
     {
         $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        // unset the owning side of the relation if necessary
+        if ($user === null && $this->user !== null) {
+            $this->user->setReferee(null);
+        }
+
+        // set the owning side of the relation if necessary
+        if ($user !== null && $user->getReferee() !== $this) {
+            $user->setReferee($this);
+        }
+
+        $this->user = $user;
 
         return $this;
     }
